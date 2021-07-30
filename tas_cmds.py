2a27a7edcd2fdb8afd3c80cf1d3e5276618e2d6f
gifford47 <razor099@gmx.de>
2021-07-30 12:47:06 +0200

    - added tasmota cmds:  status 2,7,11,12

--- tas_cmds.py
+++ tas_cmds.py
@@ -1,11 +1,14 @@
 # all device status related cmds here:
 status = {'state':'status',
     'dev_para':'status 1',
+    'fw': 'status 2',
     'memory':'status 4',
     'network':'status 5',
-    'fw':'status 2',
     'mqtt':'status 6',
+    'time':'status 7',
     'sensor':'status 8',
+    'sts':'status 11',
+    'stk':'status 12',
     'wifi':'ssid',
     'pass':'password',
     'ftopic':'FullTopic'}
